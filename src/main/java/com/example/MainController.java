acf82e06baa69fd967d70f29d0c62b60f9007ef9
Erős István <you@example.com>
2025-05-28 20:17:58 +0200

    A hozzáadás kezdetlegesen működik

--- src/main/java/com/example/MainController.java
+++ src/main/java/com/example/MainController.java
@@ -6,10 +6,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class MainController {
 
@@ -44,11 +46,15 @@ public class MainController {
     private TextField nameField;
 
     @FXML
-    private TableView<Racer> raceTable;
+    private TableView<Racer> racerTable;
 
     @FXML
     void initialize() {
-        
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        birthCol.setCellValueFactory(new PropertyValueFactory<>("birth"));
+        categoryCol.setCellValueFactory(new PropertyValueFactory<>("category"));
+        absoluteCol.setCellValueFactory(new PropertyValueFactory<>("absolute"));
     }
 
     @FXML
@@ -58,7 +64,13 @@ public class MainController {
 
     @FXML
     void onClickAddButton(ActionEvent event) {
-
+        Racer racer = new Racer();
+        racer.setName(nameField.getText());
+        racer.setBirth(birthPicker.getValue());
+        String cat = ((RadioButton) category.getSelectedToggle()).getId();
+        racer.setCategory(cat);
+        racer.setAbsolute(absoluteCheck.isSelected());
+        racerTable.getItems().add(racer);
     }
 
     @FXML
